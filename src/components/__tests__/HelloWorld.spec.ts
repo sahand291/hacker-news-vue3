430427e8dfcdea72fedff7a355d06dcc8981b602
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2024-05-04 17:06:33 +0330

    write some init tests

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -8,8 +8,11 @@ describe("HelloWorld", () => {
     const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
     expect(wrapper.text()).toContain("Hello Vitest");
   });
-  it("there is HelloWorld cmp"),
-    () => {
-      console.log(HelloWorld);
-    };
+  it("there is HelloWorld cmp", async () => {
+    const wrapper = mount(HelloWorld, {
+      shallow: true,
+      props: { msg: "dsds" },
+    });
+    expect(wrapper.text()).toContain("dsds");
+  });
 });
