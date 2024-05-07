912345b1bb4e59f4be97baa408040d84d183b57e
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2024-05-07 22:54:29 +0330

    remove tests

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,18 +1,5 @@
 import { describe, it, expect } from "vitest";
 
 import { mount } from "@vue/test-utils";
-import HelloWorld from "../HelloWorld.vue";
 
-describe("HelloWorld", () => {
-  it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
-  });
-  it("there is HelloWorld cmp", async () => {
-    const wrapper = mount(HelloWorld, {
-      shallow: true,
-      props: { msg: "dsds" },
-    });
-    expect(wrapper.text()).toContain("dsds");
-  });
-});
+describe("HelloWorld", () => {});
