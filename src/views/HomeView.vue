d1a120c0e46d6f35210797254c46dd1b2be64e3a
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2024-05-07 22:53:31 +0330

    rename to Item.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import Item from "../components/Item.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Item />
   </main>
 </template>
