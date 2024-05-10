c951e4f6804359e178c2fb026b2b007c18b83ef9
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2024-05-10 18:25:14 +0330

    test link item rendered correctly

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  item: { url: string; title: string };
+}>();
+</script>
 
 <template>
-  <p>welcome</p>
+  <li>
+    {{ props.item.url }}
+    <a :href="props.item.url">{{ props.item.title }}</a>
+  </li>
 </template>
--- src/components/__tests__/Item.spec.ts
+++ src/components/__tests__/Item.spec.ts
@@ -6,12 +6,39 @@ import Item from "../Item.vue";
 describe("Item.vue", () => {
   it("renders item.url", () => {
     const item = {
-      url: 10,
+      url: "10",
+      title: "url title",
     };
     const wrapper = mount(Item, {
       shallow: true,
       props: { item },
     });
     expect(wrapper.text()).toContain(item.url);
+  });
+
+  it("renders item.title", () => {
+    const item = {
+      url: "10",
+      title: "url title",
+    };
+    const wrapper = mount(Item, {
+      shallow: true,
+      props: { item },
+    });
+    expect(wrapper.find("a").text()).toContain(item.title);
+  });
+
+  it("render an url with link to item.url and title of item.title", () => {
+    const item = {
+      url: "www.google.com",
+      title: "google",
+    };
+    const wrapper = mount(Item, {
+      shallow: true,
+      props: { item },
+    });
+    const a = wrapper.find("a");
+    expect(a.text()).toContain("google");
+    expect(a.attributes().href === item.url).toBe(true);
   });
 });
